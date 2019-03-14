45b983360cf6ce0ab084af1cdbecadb62c641d73
jgalla <jessejgallagher@gmail.com>
2019-03-13 22:06:16 -0400

    Add additional relationships to models
    - Add `has_many :cocktail_ingredient` to `ingredient.rb`
    - Add `has_many :cocktail_ingredient` to `cocktail.rb`
    - Above changes allow data to be seeded using db:seed

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,3 +1,4 @@
 class Cocktail < ApplicationRecord
+  has_many :cocktail_ingredient
   has_many :ingredients, through: :cocktail_ingredient
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,5 @@
 class Ingredient < ApplicationRecord
+  has_many :cocktail_ingredient
   has_many :users, through: :ingredient_user
   has_many :cocktails, through: :cocktail_ingredient
 end
